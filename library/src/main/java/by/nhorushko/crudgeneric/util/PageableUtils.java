9970887035af9ba90c60dc62e36a373847b0da6e
nikolaynn <kolya.by@mail.ru>
2021-02-01 15:53:05 +0300

    refactoring

--- library/src/main/java/by/nhorushko/crudgeneric/util/PageableUtils.java
+++ library/src/main/java/by/nhorushko/crudgeneric/util/PageableUtils.java
@@ -7,10 +7,14 @@ import java.util.Map;
 
 public class PageableUtils {
 
+    public static Pageable buildPageRequest(int page, int size, String sort) {
+        return PageRequestBuilder.getPageRequest(size, page, sort);
+    }
+
     public static Pageable buildPageRequest(int page, int size, String sort, Map<String, String> entityFieldPaths) {
         for (String k : entityFieldPaths.keySet()) {
             sort.replaceAll(k, entityFieldPaths.get(k));
         }
-        return PageRequestBuilder.getPageRequest(size, page, sort);
+        return buildPageRequest(page, size, sort);
     }
 }
