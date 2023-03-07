beb11a19eb38c9c5514ec1baf9b24d749ccd7de0
Nikolay <nikolay.horushko@gmail.com>
2023-03-07 12:04:14 +0300

    AbsServiceRUD add prevUpdate hook

--- library/src/main/java/by/nhorushko/crudgeneric/v2/service/AbsServiceRUD.java
+++ library/src/main/java/by/nhorushko/crudgeneric/v2/service/AbsServiceRUD.java
@@ -41,10 +41,18 @@ public abstract class AbsServiceRUD<
 
     private DTO runUpdate(DTO dto) {
         checkId(dto);
-        ENTITY actual = repository.save(mapper.toEntity(dto));
+        ENTITY prevValue = repository.getOne(dto.getId());
+        ENTITY newValue = mapper.toEntity(dto);
+        preUpdate(prevValue, newValue);
+        ENTITY actual = repository.save(newValue);
         return mapper.toDto(actual);
     }
 
+    /**
+     * Здесь можно внести изменения в newValue, на основании предыдущего значения
+     * @param prevValue
+     * @param newValue
+     */
     protected void preUpdate(ENTITY prevValue, ENTITY newValue) {
 
     }
