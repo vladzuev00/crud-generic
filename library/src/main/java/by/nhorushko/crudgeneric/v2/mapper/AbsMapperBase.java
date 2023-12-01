b2c0e7e6d3f2826c44114f5a3f15e720a0298987
Vlad Zuev <vladzuev.00@mail.ru>
2023-12-01 12:46:43 +0300

    refactor AbsMapperBase

--- library/src/main/java/by/nhorushko/crudgeneric/v2/mapper/AbsMapperBase.java
+++ library/src/main/java/by/nhorushko/crudgeneric/v2/mapper/AbsMapperBase.java
@@ -2,40 +2,32 @@ package by.nhorushko.crudgeneric.v2.mapper;
 
 import org.modelmapper.ModelMapper;
 
-import java.util.Collection;
 import java.util.List;
 
-import static java.util.stream.Collectors.toList;
+import static by.nhorushko.crudgeneric.util.CollectionUtil.mapToList;
 
 /**
  * Mapping for objects without Id
  * https://stackoverflow.com/questions/44534172/how-to-customize-modelmapper
  */
-public abstract class AbsMapperBase<FROM, TO> {
+public abstract class AbsMapperBase<SOURCE, RESULT> {
+    private final ModelMapper modelMapper;
+    private final Class<SOURCE> sourceClass;
+    private final Class<RESULT> resultClass;
 
-    protected final ModelMapper modelMapper;
-    protected final Class<FROM> entityClass;
-    protected final Class<TO> dtoClass;
-
-    public AbsMapperBase(ModelMapper modelMapper, Class<FROM> entityClass, Class<TO> dtoClass) {
+    public AbsMapperBase(final ModelMapper modelMapper,
+                         final Class<SOURCE> sourceClass,
+                         final Class<RESULT> resultClass) {
         this.modelMapper = modelMapper;
-        this.entityClass = entityClass;
-        this.dtoClass = dtoClass;
+        this.sourceClass = sourceClass;
+        this.resultClass = resultClass;
     }
 
-    protected  <T> T map(Object source, Class<T> destinationType) {
-        if (source == null) {
-            return null;
-        }
-        return modelMapper.map(source, destinationType);
+    public final RESULT map(final SOURCE source) {
+        return source != null ? this.modelMapper.map(source, this.resultClass) : null;
     }
 
-    protected  <T> List<T> mapAll(Collection<?> source, Class<T> destinationType) {
-        if (source == null) {
-            return null;
-        }
-        return source.stream()
-                .map(o -> map(o, destinationType))
-                .collect(toList());
+    public final List<RESULT> map(final List<SOURCE> sources) {
+        return sources != null ? mapToList(sources, this::map) : null;
     }
 }
